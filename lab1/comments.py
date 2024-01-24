115101e9fe81c5d1254642b773c725d1dc827f42
yellowishcloud <156778643+yellowishcloud@users.noreply.github.com>
2024-01-24 20:31:40 +0600

    lab1

---
+++ lab1/comments.py
@@ -0,0 +1,11 @@
+#ex1
+#This is a comment
+
+#ex
+"""
+This is a comment
+written in 
+more than just one line
+
+"""
+
---
+++ lab1/syntax.py
@@ -0,0 +1,5 @@
+#ex1
+print("hello world")
+#ex2
+if 5 > 2 :
+    print("YES")
